222ed3de197ea484a7284ab5948a130941049c31
1g0rbm <m1g0rb89@gmail.com>
2020-10-04 14:41:50 +0300

    Add doctrine

--- api/bin/cli.php
+++ api/bin/cli.php
@@ -4,6 +4,9 @@
 declare(strict_types=1);
 
 use DI\Container;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 
@@ -19,6 +22,13 @@ $cli = new Application('Console');
  * @psalm-suppress MixedArrayAccess
  */
 $commands = $container->get('config')['console']['commands'];
+
+/** @var EntityManagerInterface $em */
+$em = $container->get(EntityManagerInterface::class);
+$cli->getHelperSet()->set(new EntityManagerHelper($em), 'em');
+
+ConsoleRunner::addCommands($cli);
+
 foreach ($commands as $name) {
     /** @var Command $command */
     $command = $container->get($name);
