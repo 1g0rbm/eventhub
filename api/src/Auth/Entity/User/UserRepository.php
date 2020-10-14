122efe64b57219b3770e6a5b95929c42e14241a7
1g0rbm <m1g0rb89@gmail.com>
2020-10-14 23:09:32 +0300

    Add user repo and flusher realization

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -4,29 +4,67 @@ declare(strict_types=1);
 
 namespace App\Auth\Entity\User;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use DomainException;
 
-interface UserRepository
+class UserRepository
 {
+    private EntityRepository $repo;
+
+    private EntityManagerInterface $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        /** @var EntityRepository $repo */
+        $repo = $em->getRepository(User::class);
+
+        $this->repo = $repo;
+        $this->em   = $em;
+    }
+
+    public function findByConfirmToken(string $token): ?User
+    {
+        /** @var User|null $user */
+        $user = $this->repo->findOneBy(['joinConfirmToken.value' => $token]);
+
+        return $user;
+    }
+
+    public function findByNewEmailToken(string $token): ?User
+    {
+        /** @var User|null $user */
+        $user = $this->repo->findOneBy(['newEmailToken.value' => $token]);
+
+        return $user;
+    }
+
+    public function findByPasswordResetToken(string $token): ?User
+    {
+        /** @var User|null $user */
+        $user = $this->repo->findOneBy(['passwordResetToken.value' => $token]);
+
+        return $user;
+    }
+
     /**
      * @param Id $id
      *
      * @return User
      * @throws DomainException
      */
-    public function getById(Id $id): User;
+    public function getById(Id $id): User
+    {
+        $user = $this->repo->find($id->getValue());
+        if ($user === null) {
+            throw new DomainException('user_not_found');
+        }
 
-    public function hasByEmail(Email $email): bool;
-
-    public function hasByNetworkIdentity(Network $networkIdentity): bool;
-
-    public function add(User $user): void;
-
-    public function findByConfirmToken(string $token): ?User;
-
-    public function findByNewEmailToken(string $token): ?User;
-
-    public function findByPasswordResetToken(string $token): ?User;
+        /** @var User $user */
+        return $user;
+    }
 
     /**
      * @param Email $email
@@ -34,9 +72,78 @@ interface UserRepository
      * @return User
      * @throws DomainException
      */
-    public function getByEmail(Email $email): User;
+    public function getByEmail(Email $email): User
+    {
+        $user = $this->findByEmail($email);
+        if ($user === null) {
+            throw new DomainException('user_not_found');
+        }
 
-    public function findByEmail(Email $email): ?User;
+        return $user;
+    }
 
-    public function remove(User $user): void;
+    public function findByEmail(Email $email): ?User
+    {
+        /** @var User $user */
+        $user = $this->repo->findBy(['email' => $email->getValue()]);
+
+        return $user;
+    }
+
+    /**
+     * @param Email $email
+     *
+     * @return bool
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
+    public function hasByEmail(Email $email): bool
+    {
+        return $this->repo
+                ->createQueryBuilder('u')
+                ->select('COUNT(u.id)')
+                ->andWhere('u.email = :email')
+                ->setParameters(
+                    [
+                        'email' => $email,
+                    ]
+                )
+                ->getQuery()
+                ->getSingleScalarResult() > 0;
+    }
+
+    /**
+     * @param Network $networkIdentity
+     *
+     * @return bool
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
+    public function hasByNetworkIdentity(Network $networkIdentity): bool
+    {
+        return $this->repo
+                ->createQueryBuilder('u')
+                ->select('COUNT(u.id)')
+                ->innerJoin('u.networks', 'n')
+                ->andWhere('n.name = :name')
+                ->andWhere('n.identity = :identity')
+                ->setParameters(
+                    [
+                        'name' => $networkIdentity->getName(),
+                        'identity' => $networkIdentity->getIdentity(),
+                    ]
+                )
+                ->getQuery()
+                ->getSingleScalarResult() > 0;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+    }
+
+    public function remove(User $user): void
+    {
+        $this->em->remove($user);
+    }
 }
