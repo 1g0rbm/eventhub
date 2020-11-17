6de9a2688c2e5ef762839700dc42d40c6c175dbe
1g0rbm <m1g0rb89@gmail.com>
2020-11-17 21:27:44 +0300

    Configure twig

--- api/config/common/twig.php
+++ api/config/common/twig.php
@@ -2,9 +2,53 @@
 
 declare(strict_types=1);
 
+use Psr\Container\ContainerInterface;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Extension\ExtensionInterface;
 use Twig\Loader\FilesystemLoader;
 
 return [
+    Environment::class => function (ContainerInterface $container): Environment {
+        /**
+         * @psalm-suppress MixedArrayAccess
+         * @psalm-var      array {
+         *  debug:bool,
+         *  template_dirs:array<string,string>,
+         *  cache_dir:string,
+         *  extension:string[]
+         * } $config
+         */
+        $config = $container->get('config')['twig'];
+
+        $loader = new FilesystemLoader();
+
+        foreach ($config['template_dirs'] as $alias => $dir) {
+            $loader->addPath($dir, $alias);
+        }
+
+        $env = new Environment(
+            $loader,
+            [
+                'cache' => $config['debug'] ? false : $config['cache_dir'],
+                'debug' => $config['debug'],
+                'strict_variables' => $config['debug'],
+                'auto_reload' => $config['debug'],
+            ]
+        );
+
+        if ($config['debug']) {
+            $env->addExtension(new DebugExtension());
+        }
+
+        foreach ($config['extension'] as $class) {
+            /** @var ExtensionInterface $ext */
+            $ext = $container->get($class);
+            $env->addExtension($ext);
+        }
+
+        return $env;
+    },
     'config' => [
         'twig' => [
             'debug' => (bool)getenv('APP_DEBUG'),
