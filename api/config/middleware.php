aa3c03aeaf898692aa1c9765df6ed9a051d655c0
1g0rbm <m1g0rb89@gmail.com>
2020-11-20 00:03:42 +0300

    Add sentry.io

--- api/config/middleware.php
+++ api/config/middleware.php
@@ -3,14 +3,8 @@
 declare(strict_types=1);
 
 use Slim\App;
-use DI\Container;
+use Slim\Middleware\ErrorMiddleware;
 
-return static function (App $app, Container $container) {
-    /** @psalm-var array{debug:bool, env:string} */
-    $config = $container->get('config');
-    $app->addErrorMiddleware(
-        $config['debug'],
-        $config['env'] !== 'test',
-        true
-    );
+return static function (App $app): void {
+    $app->add(ErrorMiddleware::class);
 };
--- api/public/index.php
+++ api/public/index.php
@@ -9,6 +9,10 @@ http_response_code(500);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+if (getenv('SENTRY_DSN')) {
+    Sentry\init(['dsn' => getenv('SENTRY_DSN')]);
+}
+
 /** @var Container $container */
 $container = require __DIR__ . '/../config/container.php';
 
