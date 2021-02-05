dfabad3221cac6e81a0106a36af5219da4b1662b
1g0rbm <m1g0rb89@gmail.com>
2021-02-05 19:57:44 +0300

    Update xdebug configuration + update several covers annotations

--- api/src/Auth/Test/Unit/Entity/User/Token/TokenValidateTest.php
+++ api/src/Auth/Test/Unit/Entity/User/Token/TokenValidateTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 
 /**
- * @covers Token
+ * @covers \App\Auth\Entity\User\Token
  */
 class TokenValidateTest extends TestCase
 {
--- api/src/Auth/Test/Unit/Entity/User/User/JoinByEmail/ConfirmTest.php
+++ api/src/Auth/Test/Unit/Entity/User/User/JoinByEmail/ConfirmTest.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 namespace App\Auth\Test\Unit\Entity\User\User\JoinByEmail;
 
 use App\Auth\Entity\User\Token;
-use App\Auth\Entity\User\User;
 use App\Auth\Test\Builder\UserBuilder;
 use DateTimeImmutable;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid;
 
 /**
- * @covers User
+ * @covers \App\Auth\Entity\User\User
  */
 class ConfirmTest extends TestCase
 {
