b02fab0911a6eb9e0ca1ae4821b4069afca6b44b
1g0rbm <m1g0rb89@gmail.com>
2021-02-19 15:11:36 +0300

    Codestyle fixes

--- cucumber/features/support/steps/page.js
+++ cucumber/features/support/steps/page.js
@@ -1,7 +1,7 @@
-const {Then, When} = require("@cucumber/cucumber")
-const {expect} = require('chai')
+const { Then, When } = require('@cucumber/cucumber')
+const { expect } = require('chai')
 
-When('I open {string} page', {wrapperOptions: {retry: 2}, timeout: 30000}, async function (uri) {
+When('I open {string} page', { wrapperOptions: { retry: 2 }, timeout: 30000 }, async function (uri) {
   return await this.page.goto('http://gateway:8080' + uri)
 })
 
