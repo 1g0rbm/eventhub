6db524764d6d1857503eadee961289b412ff0db6
1g0rbm <m1g0rb89@gmail.com>
2021-03-12 15:52:12 +0300

    Update dependencies

--- api/config/common/doctrine.php
+++ api/config/common/doctrine.php
@@ -28,7 +28,7 @@ return [
          *     cache_dir:?string,
          *     types:array<string,string>,
          *     subscribers:string[],
-         *     connection:array
+         *     connection:array<string, mixed>
          * } $settings
          */
         $settings = $container->get('config')['doctrine'];
